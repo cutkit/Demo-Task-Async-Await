a169fea8d7ce9871dac9aa6d1e27108f7258eaf2
cutkit49 <tungld702@gmail.com>
2018-11-09 19:08:39 +0700

    Thêm Chữ s

--- Demo-Task-Async-Await/Program.cs
+++ Demo-Task-Async-Await/Program.cs
@@ -21,7 +21,7 @@ namespace Demo_Task_Async_Await
         static async Task ShowLenghOfFile()
         {
             Console.WriteLine("Start 1");
-            int lengh = await ReadFile();// Từ khóa await cho phép chờ đợi kết quả để thực hiện các câu lệnh phía sau nó
+            int lengh = await ReadFile();//s Từ khóa await cho phép chờ đợi kết quả để thực hiện các câu lệnh phía sau nó
             //và ở đâu gọi tới hàm này sẽ cho phép nó chạy tiếp các câu lệnh phía sau mà không phải chờ hàm này thực hiện xong
             //
             Console.WriteLine("Start 3 \n"+ lengh);
